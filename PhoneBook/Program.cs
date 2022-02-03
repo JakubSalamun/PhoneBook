bc88d476cac67751b6adc24ca5509cb2ba0569d0
Askalt <jakub.salamun98@gmail.com>
2022-02-03 21:16:41 +0100

    add_kontakt_

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PhoneBook
 {
@@ -6,7 +7,44 @@ namespace PhoneBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Witaj, prosze wybrać co chcesz zrobić:");
+            Console.WriteLine("1-Dodać kontakt");
+            Console.WriteLine("2-Wyświetl numer(po podaniu numeru)");
+            Console.WriteLine("3-Wyświtl wszystkie kontakty");
+            Console.WriteLine("4-Wyświetl kontakt(po podaniu numeru)");
+            string userInput = Console.ReadLine();
+
+
+            List<Kontakt> kontakt_list = new List<Kontakt>();
+            while (true)
+            {
+
+
+                switch (userInput)
+                {
+
+                    case "1":
+
+
+                        Console.WriteLine("Podaj dane:");
+                        Console.WriteLine("Nazwa:");
+                        string userInputName = Console.ReadLine();
+
+                        Console.WriteLine("Numer:");
+                        string userInputNumber = Console.ReadLine();
+                        int help_number = int.Parse(userInputNumber);
+
+                        kontakt_list.Add(new Kontakt(userInputName, help_number));
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+
+
+
         }
     }
 }
